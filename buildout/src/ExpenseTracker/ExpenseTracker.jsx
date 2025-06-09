393621d8928e71bd7a98699313ce06d85b6cd38f
Mahima <priyanshisrivastava708016@gmail.com>
2025-06-09 13:03:13 +0530

    add

--- buildout/src/ExpenseTracker/ExpenseTracker.jsx
+++ buildout/src/ExpenseTracker/ExpenseTracker.jsx
@@ -1,19 +1,51 @@
 import style from "./ExpenseTracker.module.css";
+import popupStyles from "./Popstyle.module.css";
+import { useState } from "react";
 export default function ExpenseTracker(){
+  // we want to show popup but css will same only under the box content change 
+  const[showPopup,setShowPopup]= useState(false);
+  const[popupType,setPopupType]= useState("");
+  const handleClickAddExpense=()=>{ 
+      setPopupType("Expenses");
+      setShowPopup(true);   
+  }
+  const handleClickAddIncome=()=>{
+      setPopupType("Balance");
+      setShowPopup(true);
+  }
     return(
         <div style={{width:"100%",height:"100%",backgroundColor:"#3B3B3B",display:"flex",flexDirection:"column",boxSizing:"border-box"}}>
           <h1 style={{color:"white",marginLeft:"30px",fontWeight:"700",fontSize:"32px"}}>Expense Tracker</h1>
           <header style={{backgroundColor:"#626262",height:"40%",marginLeft:"32px",marginRight:"32px",display:"flex",flexWrap:"wrap",justifyContent:"space-between"}}>
           <div style={{height:"100%",width:"70%",display:"flex",justifyContent:"space-around"}}>
             <div style={{height:"80%",width:"35%",backgroundColor:"#9B9B9B",margin:"20px",borderRadius:"15px",display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
-              <h1 className={style.heading}>Wallet Balance: <span style={{color:"#B5DC52"}}>Rs.4500</span></h1>
-              <button style={{backgroundColor:"#B5DC52",width:"167px",height:"38px",borderRadius:"15px",fontSize:"16px",fontWeight:"700",color:"white",border:"none"}}>+Add income</button>
+              <h2 className={style.heading}>Wallet Balance: <span style={{color:"#B5DC52"}}>$0.00</span></h2>
+              <button onClick={handleClickAddIncome} type="button" labeled="+ Add Income" style={{cursor:"pointer",backgroundColor:"#B5DC52",width:"167px",height:"38px",borderRadius:"15px",fontSize:"16px",fontWeight:"700",color:"white",border:"none"}}>+Add Income</button>
             </div>
             <div style={{height:"80%",width:"35%",backgroundColor:"#9B9B9B",margin:"20px",borderRadius:"15px",display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
-              <h1 className={style.heading}>Expenses: <span style={{color:"###F4BB4A"}}>Rs.500</span></h1>
-              <button style={{backgroundColor:"#FF4747",width:"167px",height:"38px",borderRadius:"15px",fontSize:"16px",fontWeight:"700",color:"white",border:"none"}}>+Add Expense</button>
+              <h2 className={style.heading}>Expenses: <span style={{color:"###F4BB4A"}}>Rs.500</span></h2>
+              <button onClick={handleClickAddExpense} type="button" labeled="+ Add Expense" style={{cursor:"pointer",backgroundColor:"#FF4747",width:"167px",height:"38px",borderRadius:"15px",fontSize:"16px",fontWeight:"700",color:"white",border:"none"}}>+Add Expense</button>
             </div>
           </div>
+          {showPopup && (
+            <div className={popupStyles.overlay}>
+              <div className={popupStyles.popup} style={{gap:"20px",display:"flex",flexDirection:"column",border:"none"}}>
+                <h2 style={{fontWeight:"700",fontSize:"30px",marginLeft:"30px"}}>Add {popupType}</h2>
+                <div style={{display:"flex",flexWrap:"wrap",justifyContent:"space-around"}}>
+                  <input className={style.input} placeholder="Title"/>
+                   <input  className={style.input} placeholder="Price"/>
+                </div>
+                 <div style={{display:"flex",flexWrap:"wrap",justifyContent:"space-around"}}>
+                  <input className={style.input} placeholder="Select Category"/>
+                   <input  className={style.input} placeholder="dd/mm/yyyy"/>
+                </div>
+                 <div style={{display:"flex",gap:"20px",marginLeft:"35px"}}>
+                  <button className={style.addbutton}>Add {popupType}</button>
+                  <button className={style.cancelbutton}>Cancel</button>
+                </div>
+              </div>
+            </div>
+          )}
                     <div style={{height:"100%",width:"29%",display:"flex",flexDirection:"column",gap:"10px"}}>
                     <div style={{backgroundColor:"white",width:"100%",height:"80%"}}></div>
                     <div style={{width:"100%",height:"5%",display:"flex",flexWrap:"wrap",color:"white",bottom:"11px",gap:"10px",alignItems:"center",justifyContent:"center"}}>
@@ -28,13 +60,13 @@ export default function ExpenseTracker(){
           </header>
           <footer style={{width:"100%",height:"55%",top:"10px",display:"flex",flexWrap:"wrap",margin:"32px",gap:"15px"}}>
             <div style={{height:"100%",width:"60%",display:"flex",flexDirection:"column"}}>
-                   <h1 className={style.footerHeading}>Recent Transactions</h1>
+                   <h2 className={style.footerHeading}>Recent Transactions</h2>
                <div style={{width:"100%",height:"100%"}}>
                 <div  style={{backgroundColor:"white",width:"100%",height:"15%",borderRadius:"10px",display:"flex",alignItems:"center",paddingLeft:"10px"}}>No transactions!</div>
                </div>
             </div>
             <div style={{height:"100%",width:"35%",display:"flex",flexDirection:"column"}}>
-               <h1 className={style.footerHeading}>Top Expenses</h1>
+               <h2 className={style.footerHeading}>Top Expenses</h2>
                <div style={{backgroundColor:"white",width:"100%",height:"100%",display:"flex",flexDirection:"column",borderRadius:"10px"}}>
                 <div style={{width:"100%",height:"10%",margin:"30px"}}>Entertainment<span  style={{marginLeft:"10px"}}>hello</span></div>
                 <div style={{width:"100%",height:"10%",margin:"30px"}}>Food<span  style={{marginLeft:"10px"}}>hello</span></div>
